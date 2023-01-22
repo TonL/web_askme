14cc0af390101173fc4b325e018288204c3106e6
84812632+TonL@users.noreply.github.com <84812632+TonL@users.noreply.github.com>
2023-01-22 15:07:57 +0300

    includes question_item and paginator

--- app/views.py
+++ app/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'index.html')
 
-def question(request): #, question_id: int):
+def question(request, question_id: int):
     # return HttpResponse(f'q_id = {question_id}')
     return render(request, 'question.html')
 
